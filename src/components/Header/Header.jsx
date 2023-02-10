84159ba26ae9fe7f037195ba19340aef2cdb78eb
Laura <lauratelqiu@gmail.com>
2023-02-10 17:59:09 -0500

    commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import crown from "../../assets/crown.png"
-import "./header.css"
+import React from "react";
+import crown from "../../assets/crown.png";
+import "./header.css";
 
 function Header() {
   return (
-    <header className="header" id='header'>
+    <header className="header" id="header">
       <div className="brand">
         <div>
           <img src={crown} width="80" />
@@ -15,10 +15,17 @@ function Header() {
       <div className="banner">
         <h1 className="banner-heading">Welcome to Royal Hotel</h1>
         <p className="banner-paragraph">Make your life luxurious</p>
-        <button className="banner-button">Check Out</button>
+        {/* <button
+          className="banner-button"
+          onClick={() => {
+            window.location.href = "#rooms";
+          }}
+        >
+          Check Out
+        </button> */}
       </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
